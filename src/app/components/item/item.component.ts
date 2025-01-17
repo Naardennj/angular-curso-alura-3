51aaeb721a3b3afc980c4363677c1d5867ca2da0
Naardennj <gabriel.angelis787@gmail.com>
2025-01-17 10:49:11 -0300

    Implementar o LocalStorage em uma aplicação Angular Utilizar o DoCheck para verificar alterações no componente

--- src/app/components/item/item.component.ts
+++ src/app/components/item/item.component.ts
@@ -29,4 +29,12 @@ export class ItemComponent implements OnInit, OnChanges {
   editarItem() {
     this.emetindoItemParaEditar.emit(this.item);
   }
+
+  checarItem() {
+    if(this.item.comprado == true){
+      this.item.comprado = false;
+    }else{
+      this.item.comprado = true;
+    }
+  }
 }
